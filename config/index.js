98e4d0fdd1bc4b5c0321fdd8c9f9c526a7da98cd
RoseSnow <respect_69@163.com>
2018-12-02 17:26:18 +0800

    更新了商城页面

--- config/index.js
+++ config/index.js
@@ -21,8 +21,8 @@ module.exports = {
     
 
     // Various Dev Server settings
-    // host: 'localhost', 
-    host: '192.168.0.104',
+    host: 'localhost', 
+    // host: '192.168.0.104',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
